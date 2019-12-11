6a8ca037efff9081ae7d574dcbbf5b34237b5cde
OmerBenGera <omerthepro126@gmail.com>
2019-12-11 23:46:09 +0200

    Added support to 1.15

---
+++ v1_15_R1/src/main/java/com/bgsoftware/wildbuster/nms/NMSAdapter_v1_15_R1.java
@@ -0,0 +1,126 @@
+package com.bgsoftware.wildbuster.nms;
+
+import com.bgsoftware.wildbuster.api.objects.BlockData;
+import net.minecraft.server.v1_15_R1.Block;
+import net.minecraft.server.v1_15_R1.BlockPosition;
+import net.minecraft.server.v1_15_R1.ChatMessageType;
+import net.minecraft.server.v1_15_R1.Chunk;
+import net.minecraft.server.v1_15_R1.EntityHuman;
+import net.minecraft.server.v1_15_R1.EntityPlayer;
+import net.minecraft.server.v1_15_R1.IBlockData;
+import net.minecraft.server.v1_15_R1.IChatBaseComponent;
+import net.minecraft.server.v1_15_R1.ItemStack;
+import net.minecraft.server.v1_15_R1.NBTTagCompound;
+import net.minecraft.server.v1_15_R1.NBTTagList;
+import net.minecraft.server.v1_15_R1.PacketPlayOutChat;
+import net.minecraft.server.v1_15_R1.PacketPlayOutMapChunk;
+import net.minecraft.server.v1_15_R1.World;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.WorldBorder;
+import org.bukkit.craftbukkit.v1_15_R1.CraftChunk;
+import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_15_R1.block.data.CraftBlockData;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.v1_15_R1.util.CraftMagicNumbers;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.UUID;
+
+@SuppressWarnings({"unused", "ConstantConditions"})
+public final class NMSAdapter_v1_15_R1 implements NMSAdapter {
+
+    @Override
+    public String getVersion() {
+        return "v1_15_R1";
+    }
+
+    @Override
+    public void setFastBlock(Location loc, BlockData blockData) {
+        World world = ((CraftWorld) loc.getWorld()).getHandle();
+        Chunk chunk = world.getChunkAt(loc.getChunk().getX(), loc.getChunk().getZ());
+        BlockPosition blockPosition = new BlockPosition(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+        chunk.setType(blockPosition, Block.getByCombinedId(blockData.getCombinedId()), false, false);
+    }
+
+    @Override
+    public void refreshChunks(org.bukkit.World bukkitWorld, List<org.bukkit.Chunk> chunksList) {
+        World world = ((CraftWorld) bukkitWorld).getHandle();
+        for(org.bukkit.Chunk bukkitChunk : chunksList){
+            Chunk chunk = ((CraftChunk) bukkitChunk).getHandle();
+            for(EntityHuman entityHuman : world.getPlayers()) {
+                ((EntityPlayer) entityHuman).playerConnection.sendPacket(new PacketPlayOutMapChunk(chunk, 65535));
+            }
+        }
+    }
+
+    @Override
+    public void sendActionBar(Player pl, String message) {
+        IChatBaseComponent chatBaseComponent = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}");
+        PacketPlayOutChat packet = new PacketPlayOutChat(chatBaseComponent, ChatMessageType.GAME_INFO);
+        ((CraftPlayer) pl).getHandle().playerConnection.sendPacket(packet);
+    }
+
+    @Override
+    public int getMaterialId(Material type) {
+        throw new RuntimeException("You cannot run getMaterialId in 1.14.X!");
+    }
+
+    @Override
+    public int getMaterialData(org.bukkit.block.Block block) {
+        World world = ((CraftWorld) block.getWorld()).getHandle();
+        IBlockData blockData = world.getType(new BlockPosition(block.getX(), block.getY(), block.getZ()));
+        return CraftMagicNumbers.toLegacyData(blockData);
+    }
+
+    @Override
+    public int getCombinedId(org.bukkit.block.Block block) {
+        World world = ((CraftWorld) block.getWorld()).getHandle();
+        BlockPosition blockPosition = new BlockPosition(block.getX(), block.getY(), block.getZ());
+        return Block.getCombinedId(world.getType(blockPosition));
+    }
+
+    @Override
+    public Object getBlockData(int combined) {
+        return CraftBlockData.fromData(Block.getByCombinedId(combined));
+    }
+
+    @Override
+    public org.bukkit.inventory.ItemStack getPlayerSkull(org.bukkit.inventory.ItemStack itemStack, String texture) {
+        ItemStack nmsItem = CraftItemStack.asNMSCopy(itemStack);
+
+        NBTTagCompound nbtTagCompound = nmsItem.hasTag() ? nmsItem.getTag() : new NBTTagCompound();
+
+        NBTTagCompound skullOwner = nbtTagCompound.hasKey("SkullOwner") ? nbtTagCompound.getCompound("SkullOwner") : new NBTTagCompound();
+
+        NBTTagCompound properties = new NBTTagCompound();
+
+        NBTTagList textures = new NBTTagList();
+        NBTTagCompound signature = new NBTTagCompound();
+        signature.setString("Value", texture);
+        textures.add(signature);
+
+        properties.set("textures", textures);
+
+        skullOwner.set("Properties", properties);
+        skullOwner.setString("Id", UUID.randomUUID().toString());
+
+        nbtTagCompound.set("SkullOwner", skullOwner);
+
+        nmsItem.setTag(nbtTagCompound);
+
+        return CraftItemStack.asBukkitCopy(nmsItem);
+    }
+
+    @Override
+    public boolean isInsideBorder(Location location) {
+        WorldBorder worldBorder = location.getWorld().getWorldBorder();
+        Location center = worldBorder.getCenter();
+        int radius = (int) worldBorder.getSize() / 2;
+        return location.getBlockX() <=(center.getBlockX() + radius) && location.getBlockX() >= (center.getBlockX() - radius) &&
+                location.getBlockZ() <= (center.getBlockZ() + radius) && location.getBlockZ() >= (center.getBlockZ() - radius);
+    }
+
+}
