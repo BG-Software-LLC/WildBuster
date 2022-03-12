a74429b5c84916d7a8f1102472c316d5ea89f3be
OmerBenGera <omerthepro126@gmail.com>
2022-03-12 15:36:22 +0200

    Added support for CoreProtect API v9

--- Hook_CoreProtect/src/main/java/com/bgsoftware/wildbuster/hooks/CoreProtectHook.java
+++ Hook_CoreProtect/src/main/java/com/bgsoftware/wildbuster/hooks/CoreProtectHook.java
@@ -38,7 +38,7 @@ public final class CoreProtectHook {
                     coreProtect.getAPI().logPlacement(offlinePlayer.getName(), location, blockData.getType(), blockData.getData());
                     break;
             }
-        } else if (coreProtect.getAPI().APIVersion() <= 8) {
+        } else if (coreProtect.getAPI().APIVersion() <= 9) {
             switch (action) {
                 case BLOCK_BREAK:
                     coreProtect.getAPI().logRemoval(offlinePlayer.getName(), location, blockData.getType(),
