5bcae05ada8e538423332da9d262b971e5869362
OmerBenGera <omerthepro126@gmail.com>
2026-06-13 11:13:06 +0300

    Adds support to 26.2

---
+++ NMS/v26_2/src/main/java/com/bgsoftware/wildbuster/nms/v26_2/NMSAdapterImpl.java
@@ -0,0 +1,55 @@
+package com.bgsoftware.wildbuster.nms.v26_2;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.properties.PropertyMap;
+import com.mojang.datafixers.util.Either;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.entity.player.PlayerSkin;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.ResolvableProfile;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.chunk.LevelChunkSection;
+import org.bukkit.craftbukkit.block.CraftBlock;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Optional;
+
+public class NMSAdapterImpl extends com.bgsoftware.wildbuster.nms.v26_2.AbstractNMSAdapter {
+
+    @Override
+    protected LevelChunkSection getChunkSectionForY(LevelChunk levelChunk, int y) {
+        return levelChunk.getSection(levelChunk.getSectionIndex(y));
+    }
+
+    @Override
+    protected BlockState getBlockState(org.bukkit.block.Block bukkitBlock) {
+        return ((CraftBlock) bukkitBlock).getBlockState();
+    }
+
+    @Override
+    public Object getBlockData(int combined) {
+        return Block.stateById(combined).asBlockData();
+    }
+
+    @Override
+    protected void setProfileForItem(ItemStack itemStack, String texture) {
+        Multimap<String, Property> properties = HashMultimap.create();
+        properties.put("textures", new Property("textures", texture));
+
+        ResolvableProfile.Partial partialProfile = new ResolvableProfile.Partial(
+                Optional.empty(), Optional.empty(), new PropertyMap(properties));
+        ResolvableProfile resolvableProfile = new ResolvableProfile.Static(Either.right(partialProfile), PlayerSkin.Patch.EMPTY);
+
+        itemStack.set(DataComponents.PROFILE, resolvableProfile);
+    }
+
+    @Override
+    public void makeItemGlow(ItemMeta itemMeta) {
+        itemMeta.setEnchantmentGlintOverride(true);
+    }
+
+}
