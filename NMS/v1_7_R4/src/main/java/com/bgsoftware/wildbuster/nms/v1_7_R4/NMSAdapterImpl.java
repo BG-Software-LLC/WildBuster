b4ecc87848d309f5247cec7a9de467ecf930d0dc
OmerBenGera <omerthepro126@gmail.com>
2026-07-21 20:23:24 +0300

    Fix IllegalAccessError in 26.2

--- NMS/v1_7_R4/src/main/java/com/bgsoftware/wildbuster/nms/v1_7_R4/NMSAdapterImpl.java
+++ NMS/v1_7_R4/src/main/java/com/bgsoftware/wildbuster/nms/v1_7_R4/NMSAdapterImpl.java
@@ -158,7 +158,7 @@ public final class NMSAdapterImpl implements NMSAdapter {
 
         nmsItem.setTag(nbtTagCompound);
 
-        return CraftItemStack.asBukkitCopy(nmsItem);
+        return CraftItemStack.asCraftMirror(nmsItem);
     }
 
     @Override
